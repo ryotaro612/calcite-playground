86944b87c937328512e77473142d419335658c5d
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2024-04-02 15:54:38 +0900

    init kotlin project

--- test/calcite_playground/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns calcite-playground.core-test
-  (:require [clojure.test :refer :all]
-            [calcite-playground.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

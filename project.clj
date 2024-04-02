64ddcb6ee8d9fee685ca46e9cda2becd6a7d1b7c
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2024-04-02 15:33:20 +0900

    init

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject calcite-playground "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 ;; https://mvnrepository.com/artifact/org.apache.calcite/calcite-core
+                 [org.apache.calcite/calcite-core "1.36.0"]]
+  :main ^:skip-aot calcite-playground.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ test/calcite_playground/core_test.clj
@@ -0,0 +1,7 @@
+(ns calcite-playground.core-test
+  (:require [clojure.test :refer :all]
+            [calcite-playground.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

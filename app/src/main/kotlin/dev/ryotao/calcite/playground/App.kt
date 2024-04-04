ffcfb7c06c3b7daffaa0241d8b3e0fcc3356130b
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2024-04-04 10:53:05 +0900

    standalone

--- app/src/main/kotlin/dev/ryotao/calcite/playground/App.kt
+++ app/src/main/kotlin/dev/ryotao/calcite/playground/App.kt
@@ -1,5 +1,6 @@
 package dev.ryotao.calcite.playground
 import org.apache.calcite.jdbc.Driver
+import java.sql.DriverManager
 
 class App {
     val greeting: String
@@ -9,6 +10,21 @@ class App {
 }
 
 fun main() {
-    val driver = Class.forName("org.apache.calcite.jdbc.Driver")
+//    val driver = Class.forName("org.apache.calcite.jdbc.Driver")
+    val driver = Class.forName("org.apache.calcite.avatica.remote.Driver")
+
+    val connection = DriverManager.getConnection("jdbc:avatica:remote:url=http://127.0.0.1:8765;serialization=protobuf")
+    val statement = connection.createStatement();
+    /*SQL文を作成する*/
+
+    val result = statement.executeQuery("show databases;");
+    while (result.next()) {
+        println(result.getString(1))
+    }
+
+    result.close()
+    statement.close()
+    connection.close()
+
     println(App().greeting)
 }
